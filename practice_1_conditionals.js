955e33effd674d9e7c0cb9b1b56b5280d08825b4
“Edward <eminaya49@gmail.com>
2023-03-31 17:00:49 -0500

    completed question 3

--- practice_1_conditionals.js
+++ practice_1_conditionals.js
@@ -17,6 +17,13 @@ if (num < 10) {
 }
 
 // 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+var num1 = 5;
+var num2 = 2;
+if (num1 < 10 && num2 < 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
 
 // 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
