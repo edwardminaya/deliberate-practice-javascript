74822f8782b7ff259ec5535f5c77b617f160a566
“Edward <eminaya49@gmail.com>
2023-03-31 16:54:44 -0500

    started practice set 1

--- practice_1_conditionals.js
+++ practice_1_conditionals.js
@@ -7,6 +7,14 @@ if (num == 10) {
 }
 
 // 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+var num = 10;
+if (num < 10) {
+  console.log(-1);
+} else if (num > 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
 
 // 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
